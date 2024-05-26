cc4ca30a67ef7c61c7ea1dc006a79fa06993d196
tontiruu <113325721+tontiruu@users.noreply.github.com>
2024-05-27 02:03:02 +0900

    エラーハンドリングの追加

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pypdf import PdfReader
 from uuid import uuid4
 from components import processing_pdf
 from components import databse_csv
+from openai import APIConnectionError
 # API configuration
 app = FastAPI()
 # another_slack_backend.include_router(api_router)
@@ -30,7 +31,7 @@ async def create_upload_file(file: UploadFile = File(...)):
       contents = await file.read()
       filename = file.filename
       if filename.split(".")[-1] != "pdf":
-          return {"id":0,"error":True,"error_message":"PDFファイルをアップロードしてください"}   
+        return {"id":0,"error":True,"error_message":"PDFファイルをアップロードしてください"}   
       id = uuid4()
       filename = f"{id}.pdf" 
       save_file = open(f"./input_pdf/{filename}","wb")
@@ -38,17 +39,22 @@ async def create_upload_file(file: UploadFile = File(...)):
       save_file.close()
       processing_pdf.read_pdf(id)
       return {"id": id, "error":False,"error_message":None}
+    except APIConnectionError:
+        print("a")
+        return {"id":0,"error":True,"error_message":"通信エラーが発生しました。ネット接続を確認してください"}   
     except:
-        return {"id":0,"error":True,"error_message":"不明なエラーが発生しました"}
+        return {"id":0,"error":True,"error_message":"不明なエラーが発生しました"}   
+
+        # return {"id":0,"error":True,"error_message":"エラーが発生しました"}
 
 @app.get("/fetch_questions")
 async def fetch_questions(id):
-    # try:
+    try:
         response = {"error":False}
         response["data"] = databse_csv.fetch_questions(id)
         return response
-    # except:
-    #     return {"error":True,"data":None}
+    except:
+        return {"error":True,"data":None}
 
 
 
