beba854b325d0ae748a73cc34e13fad8eb0f1433
tontiruu <113325721+tontiruu@users.noreply.github.com>
2024-05-27 16:09:12 +0900

    test

--- main.py
+++ main.py
@@ -58,6 +58,6 @@ async def fetch_questions(id):
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8001)
